eead9c84f0537a08772fcd74e15e2e28211d6d1a
Jenny Gallegos <dev.jenny.ga@gmail.com>
2024-11-21 11:36:11 -0600

    [UPDATE] Data Source creation, layout, cell register

--- Gridflix/Components/Controller/OrthogonalCollectionViewController.swift
+++ Gridflix/Components/Controller/OrthogonalCollectionViewController.swift
@@ -6,8 +6,12 @@
 //
 import UIKit
 
+protocol Diffable: AnyObject {
+    
+}
+
 @MainActor
-final class OrthogonalCollectionViewController<SectionType: Hashable & Sendable, DataType: Hashable & Sendable>: UIViewController {
+final class OrthogonalCollectionViewController<SectionType: Hashable & Sendable, DataType: Hashable & Sendable>: UIViewController, UICollectionViewDelegate {
     // MARK: - Typealias
     typealias DataSource = UICollectionViewDiffableDataSource<SectionType, DataType>
     typealias Snapshot = NSDiffableDataSourceSnapshot<SectionType, DataType>
@@ -15,21 +19,27 @@ final class OrthogonalCollectionViewController<SectionType: Hashable & Sendable,
     // MARK: - Properties
     var dataSource: DataSource?
     var collectionView: UICollectionView?
-    private var elements: [String: Set<String>] = [:]
+    var coordinator: any Coordinator
+    private var sections: [HomeSection] = []
+    private var elements: [HomeSection: Set<SectionType>] = [:]
+    private var models: [SectionType] = []
 
-    var totalNumberOfSections: Int {
-        elements.keys.count
-    }
-
-    var numberOfItemsPerSection: Int {
-        30
-    }
+    var numberOfItemsPerSection: Int { 30 }
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureHierarchy()
         configureDataSource()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("Deprecated in favour of dependency injection")
+    }
+
+    init(with coordinator: any Coordinator) {
+        self.coordinator = coordinator
+        super.init(nibName: nil, bundle: nil)
     }
 }
 
@@ -39,10 +49,14 @@ extension OrthogonalCollectionViewController {
         let collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
         collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         collectionView.backgroundColor = .systemBackground
-        collectionView.register(HomeHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HomeHeaderView.identifier)
-        collectionView.register(SectionTitleHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SectionTitleHeaderView.identifier)
-        // TODO: - Register Collection View Cell and Collection View Section Headers
+        collectionView.register(HomeHeaderView.self, forSupplementaryViewOfKind: OrthogonalCollectionElementKind.layoutHeader, withReuseIdentifier: HomeHeaderView.identifier)
+        collectionView.register(SectionTitleHeaderView.self, forSupplementaryViewOfKind: OrthogonalCollectionElementKind.sectionHeader, withReuseIdentifier: SectionTitleHeaderView.identifier)
+        collectionView.register(CarouselItemView.self, forCellWithReuseIdentifier: CarouselItemView.identifier)
+        collectionView.backgroundColor = .black
+        // TODO: - Add Ranked Title Cells
+        // TODO: - Add Spotlight Title Cells
         view.addSubview(collectionView)
+        collectionView.delegate = self
         // TODO: - Set up the delegate if needed
         self.collectionView = collectionView
     }
@@ -52,19 +66,33 @@ extension OrthogonalCollectionViewController {
         
         dataSource = DataSource(collectionView: collectionView) { collectionView, indexPath, itemIdentifier in
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CarouselItemView.identifier, for: indexPath) as? CarouselItemView else { return UICollectionViewCell() }
-//            cell.configure(with: CarouselModel()) // TODO: - Fix the configure part with a Cell used for both games and movies, add styles!
+            // TODO: - Add generic configure
+//            cell.configure(with: models[indexPath.row])
             return cell
         }
-        dataSource?.supplementaryViewProvider = { collectionView, sectionIdentifier, sectionIndex in
-            guard sectionIndex.row == 0 else {
-                // TODO: - Change the title according to the sectionIndex
-                let header = SectionTitleHeaderView()
-                header.configure(with: sectionIdentifier)
+        dataSource?.supplementaryViewProvider = { [weak self] collectionView, sectionIdentifier, sectionIndex in
+            switch sectionIdentifier {
+            case OrthogonalCollectionElementKind.layoutHeader:
+                guard let header = collectionView.dequeueReusableSupplementaryView(
+                    ofKind: sectionIdentifier,
+                    withReuseIdentifier: HomeHeaderView.identifier,
+                    for: sectionIndex) as? HomeHeaderView
+                else { return UICollectionReusableView() }
+                // TODO: - Add a source to get the highlighted titles
+                // header.configure(with: ...)
                 return header
+            case OrthogonalCollectionElementKind.sectionHeader:
+                guard let header = collectionView.dequeueReusableSupplementaryView(
+                    ofKind: sectionIdentifier,
+                    withReuseIdentifier: SectionTitleHeaderView.identifier,
+                    for: sectionIndex) as? SectionTitleHeaderView
+                else { return UICollectionReusableView() }
+                guard let sections = self?.sections else { return header }
+                header.configure(with: sections[sectionIndex.section].rawValue)
+                return header
+            default:
+                return UICollectionReusableView()
             }
-            let header = HomeHeaderView()
-            // TODO: - Add Model according to the section
-            return header
         }
     }
 
@@ -74,21 +102,37 @@ extension OrthogonalCollectionViewController {
 
         let layout = UICollectionViewCompositionalLayout(sectionProvider: { sectionIndex, layoutEnvironment in
             let itemSize = NSCollectionLayoutSize(
-                widthDimension: .fractionalWidth(1.0 / 3.0),
-                heightDimension: .fractionalHeight(1.0))
+                widthDimension: .fractionalWidth(3.0 / 9.0),
+                heightDimension: .fractionalHeight(3.0 / 4.0))
             let repeatingItem = NSCollectionLayoutItem(layoutSize: itemSize)
-            repeatingItem.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
+            repeatingItem.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16)
             let groupSize = NSCollectionLayoutSize(
-                widthDimension: .fractionalWidth(85.0 / 100.0),
-                heightDimension: .fractionalHeight(1.0 / 3.0))
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(3.0 / 10.0))
             let group: NSCollectionLayoutGroup
             if #available(iOS 16.0, *) {
-                group = .horizontal(layoutSize: groupSize, repeatingSubitem: repeatingItem, count: self.numberOfItemsPerSection)
+                group = .horizontal(layoutSize: groupSize, repeatingSubitem: repeatingItem, count: 3)
             } else {
-                group = .horizontal(layoutSize: groupSize, subitem: repeatingItem, count: self.numberOfItemsPerSection)
+                group = .horizontal(layoutSize: groupSize, subitem: repeatingItem, count: 3)
             }
-            // TODO: - Add check to the SectionIndex to see what kind of layout to use
+            // TODO: - Ensure the Banner is always Section Index 0, add a mechanism to handle it
             let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .continuous
+            section.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16)
+            let sectionHeader = sectionIndex == 0
+            ? NSCollectionLayoutBoundarySupplementaryItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .fractionalHeight(9.0 / 10.0)),
+                elementKind: OrthogonalCollectionElementKind.layoutHeader,
+                alignment: .top)
+            : NSCollectionLayoutBoundarySupplementaryItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .estimated(45)),
+                elementKind: OrthogonalCollectionElementKind.sectionHeader,
+                alignment: .top)
+            section.boundarySupplementaryItems = [sectionHeader]
             return section
         }, configuration: configuration) // TODO: - Update the configuration
 
@@ -97,7 +141,6 @@ extension OrthogonalCollectionViewController {
 }
 
 struct OrthogonalCollectionElementKind {
-    static let sectionBanner = "section-banner-element-kind"
     static let sectionHeader = "section-header-element-kind"
     static let layoutHeader  = "layout-header-element-kind"
 }
